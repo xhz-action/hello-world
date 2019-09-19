53b5268a18790253646bb95d60e72bc68ae68536
xiehaizhao <282523342@qq.com>
2019-09-19 19:23:22 +0800

    初始代码

---
+++ src/main/java/jenkins/controller/IndexController.java
@@ -0,0 +1,15 @@
+package jenkins.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/jenkins")
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
